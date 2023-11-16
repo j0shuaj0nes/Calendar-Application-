cfad58625ca826ed7ee31ad1ee7d9f7ecd6fbbe0
Josh Jones <9joshuajones5@gmail.com>
2023-11-16 22:11:12 +0800

    adding updated javascript file and readme file

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Calendar-Application-
-Calendar application that allows a user to save events for each hour of a typical working day (9am–5pm). 
+
+## Description
+
+This simple calendar application enables users to plan their typical working day (9am-5pm) by saving events for each hour. Local storage persists the saved events when the application is closed or refreshed and reopened. 
+The purpose and motivation behind this project was offer a streamline and easy way for users to plan their daily event schedule.
+Users are able to keep track of appointments, meeting and events. This app aids users to keep track of upcoming daily happenings. 
+
+## Usage
+
+To use this app, first open the calendar application and the user will be presented with a series of time blocks for a normal working day, starting at 9am and finishing at 5pm. 
+
+[Intitial Display](./images/Screenshot%202023-11-16%20at%209.51.37 pm.png)
+
+Users are then able to select a desired time block in their working day and enter an event in the selected hour. 
+
+[Enter Event](./images/Screenshot%202023-11-16%20at%209.52.18 pm.png)
+
+The user can then save the scheduled event by clicking on the save icon to the right of the desired timeblock. 
+
+[Save Event](./images/Screenshot%202023-11-16%20at%209.52.41 pm.png)
+
+If the page is refreshed or the user refreshes their page, the scheduled event will persist, unless the user alters or removes the content of the event in their schedule. Any removal or alteration of an event must also be saved or the previous content will reapply when the page is reloaded again. 
+
+[Reload or Refresh Page](./images/Screenshot%202023-11-16%20at%209.54.16 pm.png)
+
+## Credits
+jQuery - https://jquery.com/
+jsDelivr - https://www.jsdelivr.com/
+Font Awesome - https://fontawesome.com/start
+Google Fonts - https://fonts.google.com/
+Programming with Mosh - https://www.youtube.com/watch?v=PFmuCDHHpwk&list=PLTjRvDozrdlxEIuOBZkMAK5uiqp8rHUax&index=3 
+
--- script.js
+++ script.js
@@ -2,22 +2,63 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  var today = dayjs();
+  var notesContainer = $('.container-lg');
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  notesContainer.on('click', '.btn', function () {
+    var currentHour = JSON.parse($(this).parent().attr('id').split('-')[1]);
+    var currentNote = $(this).parent().children('.description').val();
+    var currentHourKey = 'hour-' + currentHour;
+    console.log(currentHourKey);
+
+    localStorage.setItem(currentHourKey, currentNote);
+  })
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  function timeBlockColourUpdater() {
+    console.log(dayjs().hour())
+    var currentHour = dayjs().hour()
+    $('.time-block').each(function (elements, index) {
+      console.log(index)
+      var timeBlockHour = JSON.parse($(this).attr('id').split('-')[1]);
+
+      var currentNote = localStorage.getItem('hour-' + timeBlockHour);
+
+      $(this).children('textarea').val(currentNote)
+      if (timeBlockHour < currentHour) {
+        $(this).addClass('past')
+      } else if (timeBlockHour === currentHour) {
+        $(this).removeClass('past')
+        $(this).addClass('present')
+      } else {
+        $(this).removeClass('past')
+        $(this).removeClass('present')
+        $(this).addClass('future')
+      }
+      console.log(timeBlockHour)
+
+    })
+
+
+  }
+  //$(this).text(localStorage.getItem('day-9'))
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  $('#currentDay').text(today.format('dddd MMMM D, YYYY'));
+  timeBlockColourUpdater()
 });
+
